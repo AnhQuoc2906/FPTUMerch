837c0190748bef2803103003e36047ff5b2f30e7
Hoàng Đình Anh Quốc <anhquoc2906@gmail.com>
2023-09-08 01:06:51 +0700

    update 08092023

--- assets/js/FPTUMerch_JS/prethanhtoan.js
+++ assets/js/FPTUMerch_JS/prethanhtoan.js
@@ -1,6 +1,8 @@
 let listCarts = JSON.parse(sessionStorage.getItem('listCarts')); // Get the temporary cart
 let discountCode = document.querySelector('#discountCode'); //GET DISCOUNT CODE
 let quantity = document.querySelector('.quantity'); // How many kinds of products are there
+let cartTop = document.querySelector('.cart-top');
+let totalPrice = document.querySelector('.totalPrice');
 let announce = document.querySelector('.announce');
 let tempPrice = document.querySelector('.tempPrice');
 let discountPrice = document.querySelector('.discountPrice');
@@ -11,13 +13,15 @@ if (JSON.parse(sessionStorage.getItem('listCarts')) == null) {
     listCarts = [];
 }
 
+totalPrice.innerHTML = "";
 let currentTotalPrice = 0;
 if (listCarts.length>0) {
     bodyCartList.innerHTML = "";
     for (let i = 0; i < listCarts.length; i++) {
         currentTotalPrice += listCarts[i].price * listCarts[i].quantity; // Calculate total price
     };
-    if (tempPrice) {
+    if (tempPrice && totalPrice) {
+        totalPrice.innerHTML += currentTotalPrice.toLocaleString() + " VND";
         quantity.innerHTML = listCarts.length;
         tempPrice.innerHTML = currentTotalPrice.toLocaleString() + " VND";
         finalPrice.innerHTML = currentTotalPrice.toLocaleString() + " VND";
@@ -62,6 +66,31 @@ if (listCarts.length>0) {
 `;
             bodyCartList.append(newDiv);
     })
+
+    listCarts.forEach((value, index) => {
+        if (value != null) {
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = `<div class="single-cart">
+                <div class="cart-img">
+                    <img alt="" src="media/images/${value.productLink}.jpg" style="max-width:100px">
+                </div>
+                <div class="cart-title">
+                    <p><a href="">${value.productName}</a></p>
+                </div>
+                <div class="cart-price">
+                    <button onclick="changeQuantity(${index}, ${parseInt(value.quantity) - 1})">-</button>
+                    <p>${value.quantity} </p>
+                    <button onclick="changeQuantity(${index}, ${parseInt(value.quantity) + 1})">+</button>
+                    <p>x ${value.price.toLocaleString()}</p>
+                </div>
+                <div class="cart-price">
+                    <p>${value.note}</p>
+                </div>
+                <a href="#"><i class="fa fa-times" onclick="removeFromCart(${index},event)"></i></a>
+                </div>`;
+            cartTop.append(newDiv);
+        }
+    })    
 } else {
     document.querySelector('.cart-table').innerHTML 
     = "Hiện bạn đang không có món hàng nào, hãy chọn ít nhất 1 món trước khi quay lại đây nhé! <br/>"
@@ -72,7 +101,11 @@ function discountCodeSubmit(e) {
     e.preventDefault();
     console.log(discountPrice.innerHTML);
     if (discountCode.value == "") {
-
+        announce.innerHTML = "Bạn chưa nhập mã, hãy thử lại";
+        announce.style.color = "red";
+        discountPrice.innerHTML = "0 VND";
+        finalPrice.innerHTML = currentTotalPrice.toLocaleString() + " VND";
+        checkDiscountCode = false;
     } else {
         fetch("https://fptumerchapi-cocsaigon.up.railway.app/api/DiscountCode/" + discountCode.value, {
             method: "GET"
@@ -120,14 +153,17 @@ function reloadCart(){
     if (quantity && tempPrice) {
         tempPrice.innerHTML = currentTotalPrice.toLocaleString() + " VND";
         quantity.innerHTML = listCarts.length;
+        totalPrice.innerHTML = currentTotalPrice.toLocaleString() + " VND";
         if(checkDiscountCode == false){
+            discountPrice.innerHTML = "0 VND";
             finalPrice.innerHTML = currentTotalPrice.toLocaleString() + " VND";
         } else{
-            discountCode.innerHTML = (currentTotalPrice/10).toLocaleString() + " VND";
+            discountPrice.innerHTML = (parseInt(currentTotalPrice / 10)).toLocaleString() + " VND";
             finalPrice.innerHTML = (currentTotalPrice*9/10).toLocaleString() + " VND";
         }
     };
     bodyCartList.innerHTML = "";
+    cartTop.innerHTML = "";
     listCarts.forEach((item, index) =>{
         let newDiv = document.createElement('tr');
             newDiv.innerHTML = `<td>
@@ -168,9 +204,46 @@ function reloadCart(){
     `;
         bodyCartList.append(newDiv);
     })
+    listCarts.forEach((value, index) => {
+        if (value != null) {
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = `<div class="single-cart">
+                <div class="cart-img">
+                    <img alt="" src="media/images/${value.productLink}.jpg" style="max-width:100px">
+                </div>
+                <div class="cart-title">
+                    <p><a href="">${value.productName}</a></p>
+                </div>
+                <div class="cart-price">
+                    <button onclick="changeQuantity(${index}, ${parseInt(value.quantity) - 1})">-</button>
+                    <p>${value.quantity} </p>
+                    <button onclick="changeQuantity(${index}, ${parseInt(value.quantity) + 1})">+</button>
+                    <p>x ${value.price.toLocaleString()}</p>
+                </div>
+                <div class="cart-price">
+                    <p>${value.note}</p>
+                </div>
+                <a href="#"><i class="fa fa-times" onclick="removeFromCart(${index},event)"></i></a>
+                </div>`;
+            cartTop.append(newDiv);
+        }
+    }) 
     sessionStorage.setItem('listCarts', JSON.stringify(listCarts));
 }
 
 function passData() {
     sessionStorage.setItem('listCarts', JSON.stringify(listCarts));
+}
+
+function changeQuantity(index, quantity) {
+    if (quantity == 0) {
+        const response = confirm("Bỏ món hàng này ra khỏi danh sách?");
+        if (response) {
+            listCarts.splice(index, 1);
+            reloadCart();
+        }
+    } else {
+        listCarts[index].quantity = quantity;
+        reloadCart();
+    }
 }
